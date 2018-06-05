8e4b07bd741a1c08389e5bff328f570d7d291d94
Michelle Monfort <Michellemnfrt@gmail.com>
2018-06-05 14:25:29 -0400

    still working

--- hof.js
+++ hof.js
@@ -2,15 +2,15 @@
 
 var people = [
   {
-    name: "Layla",
+    name: 'Layla',
     age: 27,
     course: 'WDI'
   }, {
-    name: "Keanu",
+    name: 'Keanu',
     age: 54,
     course: 'WDI'
   }, {
-    name: "Jasmine",
+    name: 'Jasmine',
     age: 35,
     course: 'DSI'
   }
@@ -21,9 +21,22 @@ var people = [
 // called `peopleAges`.
 // Type your solution immediately below this line:
 
+var peopleAges =
+people.map(function (person) {
+  return person.ages
+})
+peopleAges.push(people.Ages)
 
+// for( let i =0; i > people.length; i++) {
+// push.peopl
+// }
 
 // #2: Use the `filter` array method to create a new, filtered array containing only
 // persons from the `people` array who are WDI students. Assign the returned array
 // to a variable called `wdiStudents`.
 // Type your solution immediately below this line:
+
+var wdiStudents = people.filter(function (person) {
+  if (person.course === 'WDI'); {
+    return name.name
+  }
